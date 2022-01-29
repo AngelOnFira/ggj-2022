3592779ba34904dc1c634333940b530ef3dd671f
Connor Hillen <connor.hillen@gmail.com>
2022-01-29 14:55:04 -0500

    Refactor cards

--- Prototype/DecisionElement.gd
+++
@@ -1,7 +0,0 @@
-extends "res://Prototype/CardBehaviour.gd"
-
-var ELEMENT_TYPE = "DecisionElement";
-
-# Don't reorder this list?
-# "Cub", "Child", "Blade", "Hunger", "Family"
-export(Array, Array, String, MULTILINE) var cards
--- Prototype/StoryElement.gd
+++
@@ -1,3 +0,0 @@
-extends "res://Prototype/CardBehaviour.gd"
-
-var ELEMENT_TYPE = "StoryElement";

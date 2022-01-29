8429ecd599c148338e64d391a5ea42b862bd5dc6
Connor Hillen <connor.hillen@gmail.com>
2022-01-29 15:58:14 -0500

    Updating the update_card to actually use cards

--- master.gd
+++ master.gd
@@ -1,12 +1,17 @@
 extends Control
 
+var PlayerCard = preload("res://Card.tsn")
+var CardContainerPath = NodePath("PlayerCards/HBoxContainer")
+var current_page
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for page in self.get_tree().get_nodes_in_group("pages"):
 		page.connect("page_changed",self,"update_page")
+
 	# Replace with function body.
-	#self.get_tree().call_group("pages","connect","page_changed",self,"update_page")
-	$Story.current_page.take_action("Continue","left")
+	# self.get_tree().call_group("pages","connect","page_changed",self,"update_page")
+	self.update_page($Story.current_page)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -17,7 +22,13 @@ func card_action(action:String,card:String):
 	pass
 
 func update_page(page):
-	print("test")
-	print(page)
-	pass
+	self.current_page = page
+
+	# Remove existing cards
+	var cards = get_node(CardContainerPath)
+	for child in cards.get_children():
+		child.queue_free()
+	
+	# TODO Add new cards then update page text
+	# for card in self.current_page
 

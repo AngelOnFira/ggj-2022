be8525363642aa4e557171fdf2a66a9bf86005ef
ChristopherBlackman <mr.christopher.blackman@gmail.com>
2022-01-28 23:38:23 -0500

    Center Text For Narrator
    - added functions to clear narrator node
    -added functions to add text to narrator node (with options to change vertical align)

--- narrator.gd
+++ narrator.gd
@@ -5,5 +5,18 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+#func _process(_delta):
+#	pass
+
+# clears field of all text
+func clear():
+	$TextLabel.text = ""
+	
+# appends text to the rich text label
+# defualt is center align
+# text : text to be appended
+# align : align option for text based on bbcode_text (defualt "center")
+func append_text(text:String ,align:String ="center"):
+	var insert_text = "[" + align + "]" + text + "[/" + align + "]"
+	$TextLabel.text = $TextLabel.text + "\n" + insert_text
 	pass

de6d0e859dbba5081f7a4ff5b9a5c72b0cd63c9b
Forest Anderson <forestkzanderson@gmail.com>
2022-01-30 13:44:46 -0500

    Fixed import for decision text

--- Import.gd
+++ Import.gd
@@ -6,6 +6,7 @@ export var run_import = false setget do_run_import
 
 var file
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -15,6 +16,7 @@ func _ready():
 # func _process(delta):
 # 	print("test2")
 # 	add_child(Node.new()).set_owner(get_tree().get_edited_scene_root())
+
 
 func import():
 	var file = File.new()
@@ -28,28 +30,26 @@ func import():
 
 	var regex = RegEx.new()
 	# regex.compile("\(\\d+)")
-	
+
 	for page in json_result.result["passages"]:
 		# For each one
 
 		# Create a node
 		var node = Node.new()
-		
-
 
 		# Set the name
 		node.name = page["name"]
-		
+
 		# Attach stript
 		node.set_script(load("res://Prototype/Page.gd"))
-		
+
 		# Set the text
 		node.text_description = page["text"]
 
 		# Add to tree
 		add_child(node)
 		node.set_owner(get_tree().get_edited_scene_root())
-		
+
 		# End of story
 		if !page.has("links"):
 			continue
@@ -59,7 +59,7 @@ func import():
 			var new_card = Node.new()
 
 			new_card.set_script(load("res://Prototype/CardComponent.gd"))
-			
+
 			new_card.left_text = "Go on..."
 			new_card.right_text = "Go on..."
 			new_card.continue_left = NodePath("../../" + page["links"][0]["link"])
@@ -79,9 +79,9 @@ func import():
 				var second_card = page["links"][i * 2 + 1]
 
 				new_card.set_script(load("res://Prototype/CardComponent.gd"))
-				
-				new_card.left_text = first_card["name"].split(")")[1]
-				new_card.right_text = second_card["name"].split(")")[1]
+
+				new_card.left_text = first_card["name"].split(")")[1].replace("'", "").replace("`", "").strip_edges()
+				new_card.right_text = second_card["name"].split(")")[1].replace("'", "").replace("`", "").strip_edges()
 				new_card.continue_left = NodePath("../../" + first_card["link"])
 				new_card.continue_right = NodePath("../../" + second_card["link"])
 				new_card.name = first_card["name"].split("(")[1].split(")")[0].strip_edges()
@@ -91,12 +91,10 @@ func import():
 				new_card.set_owner(get_tree().get_edited_scene_root())
 
 
-		
-
 func do_run_import(val):
 	if run_import == val:
-	    return
+		return
 	elif val:
-	    import()
-		
+		import()
+
 	run_import = val

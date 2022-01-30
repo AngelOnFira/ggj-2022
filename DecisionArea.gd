60db829cac6d76c77f8e67fc9d9d011e510163ea
Connor Hillen <connor.hillen@gmail.com>
2022-01-30 14:05:05 -0500

    Particles on hover

--- DecisionArea.gd
+++ DecisionArea.gd
@@ -1,22 +1,27 @@
 extends Control
 
 export var decision: String = "default"
+var showing_text = false
 
 
 func _on_DecisionArea_mouse_entered():
 	self.get_tree().call_group("MouseCards", "enter_area", self.decision, self)
+	self.get_node("CPUParticles2D").emitting = true
 
 
 func _on_DecisionArea_mouse_exited():
 	self.get_tree().call_group("MouseCards", "exit_area", self)
+	self.get_node("CPUParticles2D").emitting = false
 
 
 func show_text():
 	$CardFlavour.show()
-
+	showing_text = true
+	
 
 func hide_text():
 	$CardFlavour.hide()
+	showing_text = false
 
 
 func set_text(text: String):
